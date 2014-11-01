38127024966a6df0a7fc545baf404086415435ac
Charles Nurse <charlesnurse@gmail.com>
2014-10-31 19:52:30 -0700

    Updates to WebForms module

--- Dnn.WebForms.Tasks/Properties/AssemblyInfo.cs
+++ Dnn.WebForms.Tasks/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dnn.WebForms.Tasks")]
+[assembly: AssemblyTitle("Dnn.SPA.Tasks")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dnn.WebForms.Tasks")]
+[assembly: AssemblyProduct("Dnn.SPA.Tasks")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
